1de22201137248179ee9e4395bb15153dccacbfe
shahid <81615526+darshahid72@users.noreply.github.com>
2024-07-07 20:52:46 +0530

    Update urls.py

--- attendance_management_system/rosters/urls.py
+++ attendance_management_system/rosters/urls.py
@@ -7,7 +7,7 @@ from .views import (AddShifts,StaffDetails,ViewShifts,MarkAttendance)
 urlpatterns = [
     path("shift/", AddShifts.as_view()),
     path("edit-staff-details/", StaffDetails.as_view()),
-    path("view-shifts/", ViewShifts.as_view()),
+    path("shifts/", ViewShifts.as_view()),
     path("markattendance/", MarkAttendance.as_view()),
 
 
